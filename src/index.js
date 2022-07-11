7861defaf53ef1439afca71baa2495b4afbe60ba
Davidfpll <olmofrancodavid@fpllefia.com>
2022-07-11 10:04:11 +0200

    markdown

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const app = express()
-const PORT = 8080
-const NOMBRE = 'David'
+
 
 dotenv.config()
 app.use(cors())
@@ -17,12 +16,13 @@ app.use(cors())
 app.get('/', (req,res)=>{
     res.status(200).json({
         error:false,
-        mensaje: 'Estas en la api de ' + nombre
+        mensaje: 'Estas en la api de ' + nombre,
+        puerto: process.env.PORT
     })
 })
 
 
 app.listen(process.env.PORT, ()=>{
     console.log('Servidor escuchando en puerto ' + 
-    PORT + ' del ordenador de ' + NOMBRE);
+    process.env.PORT + ' del ordenador de ' + NOMBRE);
 })
